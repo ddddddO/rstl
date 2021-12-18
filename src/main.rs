6125b0dfd56eae2404b3fd863a40e53ead211797
ddddddO <lbfdeatq@gmail.com>
2021-12-19 03:04:39 +0900

    コマンドライン引数を受け付ける

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::net::TcpStream;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let mut remote: String = "".to_string();
 
-    if let Ok(_stream) = TcpStream::connect("127.0.0.1:1123") {
+    if args.len() == 3 {
+        let host = &args[1];
+        let port = &args[2];
+        remote = format!("{}:{}", host, port);
+    } else if args.len() == 2 {
+        let host = &args[1];
+        let default_port = "23".to_string();
+        remote = format!("{}:{}", host, default_port);
+    } else {
+        println!("args error");
+    }
+
+    if let Ok(_stream) = TcpStream::connect(remote) {
         println!("Connected to the server!");
     } else {
         println!("Couldn't connect to server...");
