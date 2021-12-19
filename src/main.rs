011b569d1a7f8a20da048361afb646da6a4776b0
ddddddO <lbfdeatq@gmail.com>
2021-12-19 10:09:08 +0900

    コマンドライン引数から接続先を組み立てる関数に切り出し

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use std::net::TcpStream;
+use std::env;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let mut remote: String = "".to_string();
-
-    if args.len() == 3 {
-        let host = &args[1];
-        let port = &args[2];
-        remote = format!("{}:{}", host, port);
-    } else if args.len() == 2 {
-        let host = &args[1];
-        let default_port = "23".to_string();
-        remote = format!("{}:{}", host, default_port);
-    } else {
-        println!("args error");
-    }
+    let args: Vec<String> = env::args().collect();
+    let remote: String = build_remote(&args);
 
     if let Ok(_stream) = TcpStream::connect(remote) {
         println!("Connected to the server!");
     } else {
         println!("Couldn't connect to server...");
+    }
+}
+
+fn build_remote(args: &Vec<String>) -> String {
+    if args.len() == 3 {
+        let host = &args[1];
+        let port = &args[2];
+        return format!("{}:{}", host, port);
+    } else if args.len() == 2 {
+        let host = &args[1];
+        let default_port = "23".to_string();
+        return format!("{}:{}", host, default_port);
+    } else {
+        // TODO: error handring
+        return "args error".to_string()
     }
 }
